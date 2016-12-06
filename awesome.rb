6c1bbec58b91f2784247181be26779f6d8d3ea86
athityakumar <athityakumar@gmail.com>
2016-12-06 15:48:35 +0530

    Init commit. Setup files

---
+++ awesome.rb
@@ -0,0 +1,10 @@
+def scrape_github link , n_pages
+
+end
+
+def create_readme filename , template
+
+end
+
+scrape_github("https://github.com/search?q=awesome",3)
+create_readme("README.md" , "README_template.erb")
